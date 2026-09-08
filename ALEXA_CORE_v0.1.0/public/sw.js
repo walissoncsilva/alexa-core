ab4e48340576d2c00ec87b79adc7a177bfa0c89a
walissoncsilva <walissonc@gmail.com>
2026-09-08 07:40:49 -0300

    ALEXA CORE v0.1.1-R4 - atualizar cache PWA

--- ALEXA_CORE_v0.1.0/public/sw.js
+++ ALEXA_CORE_v0.1.0/public/sw.js
@@ -1,9 +1,12 @@
-const CACHE = 'alexa-core-v010';
+const CACHE = 'alexa-core-v011-r4';
 const ASSETS = ['/', '/index.html', '/style.css', '/app.js', '/manifest.webmanifest'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   e.respondWith(fetch(e.request).then(r => {
-    const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r;
+    const copy = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return r;
   }).catch(() => caches.match(e.request)));
 });
